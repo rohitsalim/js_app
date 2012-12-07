352516b80bfd3a3f3fcd625dcd178e83fd6de71f
Rohith Salim <rohitsalim@gmail.com>
2012-12-07 14:15:30 -0500

    Trying to get the js app to work as a Ruby app

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require "sinatra"
 require "koala"
-require "thin"
 
+FACEBOOK_SCOPE = 'user_likes,user_photos,user_photo_video_tags'
+
+unless ENV["FACEBOOK_APP_ID"] && ENV["FACEBOOK_SECRET"]
+	abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
+end
+
+# Scope defines what permissions that we are asking
 
 get "/" do
-  erb :jssdkouth2
+	@app_id = ENV["FACEBOOK_APP_ID"]
+	erb :jssdkouth2
 end
